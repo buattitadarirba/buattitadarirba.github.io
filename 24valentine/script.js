305ed8718409a62009c6e47f52b0851730423c7e
ryandhikabaa <ryandhikabintang0@gmail.com>
2024-02-14 00:37:58 +0700

    valentine24

--- 24valentine/script.js
+++ 24valentine/script.js
@@ -1,92 +1,92 @@
-var TxtType = function(el, toRotate, period) {
-    this.toRotate = toRotate;
-    this.el = el;
-    this.loopNum = 0;
-    this.period = parseInt(period, 10) || 2000;
-    this.txt = '';
-    this.tick();
-    this.isDeleting = false;
+var TxtType = function (el, toRotate, period) {
+  this.toRotate = toRotate;
+  this.el = el;
+  this.loopNum = 0;
+  this.period = parseInt(period, 10) || 2000;
+  this.txt = '';
+  this.tick();
+  this.isDeleting = false;
 };
 
-TxtType.prototype.tick = function() {
-    var i = this.loopNum % this.toRotate.length;
-    var fullTxt = this.toRotate[i];
+TxtType.prototype.tick = function () {
+  var i = this.loopNum % this.toRotate.length;
+  var fullTxt = this.toRotate[i];
 
-    if (this.isDeleting) {
+  if (this.isDeleting) {
     this.txt = fullTxt.substring(0, this.txt.length - 1);
-    } else {
+  } else {
     this.txt = fullTxt.substring(0, this.txt.length + 1);
-    }
+  }
 
-    this.el.innerHTML = '<span class="wrap">'+this.txt+'</span>';
+  this.el.innerHTML = '<span class="wrap">' + this.txt + '</span>';
 
-    var that = this;
-    var delta = 200 - Math.random() * 100;
+  var that = this;
+  var delta = 200 - Math.random() * 100;
 
-    if (this.isDeleting) { delta /= 2; }
+  if (this.isDeleting) { delta /= 2; }
 
-    if (!this.isDeleting && this.txt === fullTxt) {
+  if (!this.isDeleting && this.txt === fullTxt) {
     delta = this.period;
     this.isDeleting = true;
-    } else if (this.isDeleting && this.txt === '') {
+  } else if (this.isDeleting && this.txt === '') {
     this.isDeleting = false;
     this.loopNum++;
     delta = 500;
-    }
+  }
 
-    setTimeout(function() {
+  setTimeout(function () {
     that.tick();
-    }, delta);
+  }, delta);
 };
 
-window.onload = function() {
-    var elements = document.getElementsByClassName('typewrite');
-    for (var i=0; i<elements.length; i++) {
-        var toRotate = elements[i].getAttribute('data-type');
-        var period = elements[i].getAttribute('data-period');
-        if (toRotate) {
-          new TxtType(elements[i], JSON.parse(toRotate), period);
-        }
+window.onload = function () {
+  var elements = document.getElementsByClassName('typewrite');
+  for (var i = 0; i < elements.length; i++) {
+    var toRotate = elements[i].getAttribute('data-type');
+    var period = elements[i].getAttribute('data-period');
+    if (toRotate) {
+      new TxtType(elements[i], JSON.parse(toRotate), period);
     }
-    // INJECT CSS
-    var css = document.createElement("style");
-    css.type = "text/css";
-    css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
-    document.body.appendChild(css);
+  }
+  // INJECT CSS
+  var css = document.createElement("style");
+  css.type = "text/css";
+  css.innerHTML = ".typewrite > .wrap { border-right: 0.08em solid #fff}";
+  document.body.appendChild(css);
 };
 
 function tampilJam() {
-            var a_p = "";
-            var hari = new Date();
-            var jam_skr = hari.getHours();
-            var menit_skr = hari.getMinutes();
-            var detik_skr = hari.getSeconds();
-            if (jam_skr < 12) {
-                a_p = "AM";
-            } else {
-                a_p = "PM";
-            }
-            if (jam_skr == 0) {
-                jam_skr = 12;
-            }
-            if (jam_skr > 12) {
-                jam_skr = jam_skr - 12;
-            }
-            jam_skr = cekJam(jam_skr);
-            menit_skr = cekJam(menit_skr);
-            detik_skr = cekJam(detik_skr);
-         document.getElementById('jam').innerHTML=jam_skr + ":" + menit_skr + ":" + detik_skr + " " + a_p;
-            }
- 
-        function cekJam(i) {
-            if (i < 10) {
-                i = "0" + i;
-            }
-            return i;
-        }
-        setInterval(tampilJam, 100);
+  var a_p = "";
+  var hari = new Date();
+  var jam_skr = hari.getHours();
+  var menit_skr = hari.getMinutes();
+  var detik_skr = hari.getSeconds();
+  if (jam_skr < 12) {
+    a_p = "AM";
+  } else {
+    a_p = "PM";
+  }
+  if (jam_skr == 0) {
+    jam_skr = 12;
+  }
+  if (jam_skr > 12) {
+    jam_skr = jam_skr - 12;
+  }
+  jam_skr = cekJam(jam_skr);
+  menit_skr = cekJam(menit_skr);
+  detik_skr = cekJam(detik_skr);
+  document.getElementById('jam').innerHTML = jam_skr + ":" + menit_skr + ":" + detik_skr + " " + a_p;
+}
 
-        var canvas = document.getElementById('container');
+function cekJam(i) {
+  if (i < 10) {
+    i = "0" + i;
+  }
+  return i;
+}
+setInterval(tampilJam, 100);
+
+var canvas = document.getElementById('container');
 var clone = document.getElementById('blurCanvasBottom');
 
 var cloneCtx = clone.getContext('2d');
@@ -99,23 +99,23 @@ var h = $('#blurCanvasTop').height();
 var ww = $(window).width();
 var wh = $(window).height();
 canvas.width = ww;
-canvas.height= wh;
+canvas.height = wh;
 var partCount = 100;
 var particles = [];
 
-function particle(){
-  this.color = 'rgba(255,255,255,'+ Math.random()+')';
+function particle() {
+  this.color = 'rgba(255,255,255,' + Math.random() + ')';
   console.log(this.color);
-  this.x = randomInt(0,ww);
-  this.y = randomInt(0,wh);
+  this.x = randomInt(0, ww);
+  this.y = randomInt(0, wh);
   this.direction = {
     "x": -1 + Math.random() * 2,
     "y": -1 + Math.random() * 2
   };
   this.vx = 0.3 * Math.random();
   this.vy = 0.3 * Math.random();
-  this.radius = randomInt(2,3);
-  this.float = function(){
+  this.radius = randomInt(2, 3);
+  this.float = function () {
     this.x += this.vx * this.direction.x;
     this.y += this.vy * this.direction.y;
   };
@@ -123,21 +123,21 @@ function particle(){
     this.direction[axis] *= -1;
   };
   this.boundaryCheck = function () {
-            if (this.x >= ww) {
-                this.x = ww;
-                this.changeDirection("x");
-            } else if (this.x <= 0) {
-                this.x = 0;
-                this.changeDirection("x");
-            }
-            if (this.y >= wh) {
-                this.y = wh;
-                this.changeDirection("y");
-            } else if (this.y <= 0) {
-                this.y = 0;
-                this.changeDirection("y");
-            }
-        };
+    if (this.x >= ww) {
+      this.x = ww;
+      this.changeDirection("x");
+    } else if (this.x <= 0) {
+      this.x = 0;
+      this.changeDirection("x");
+    }
+    if (this.y >= wh) {
+      this.y = wh;
+      this.changeDirection("y");
+    } else if (this.y <= 0) {
+      this.y = 0;
+      this.changeDirection("y");
+    }
+  };
   this.draw = function () {
     ctx.beginPath();
     ctx.fillStyle = this.color;
@@ -146,57 +146,55 @@ function particle(){
   };
 }
 function clearCanvas() {
- cloneCtx.clearRect(0, 0, ww, wh);
- ctx.clearRect(0, 0, ww, wh);
+  cloneCtx.clearRect(0, 0, ww, wh);
+  ctx.clearRect(0, 0, ww, wh);
 }
-function createParticles(){
-  for (i=0;i<partCount;i++){
+function createParticles() {
+  for (i = 0; i < partCount; i++) {
     var p = new particle();
     particles.push(p);
   }
 }
 function drawParticles() {
-   for (i=0;i<particles.length;i++) {
-     p = particles[i];
-     p.draw();
-   }
+  for (i = 0; i < particles.length; i++) {
+    p = particles[i];
+    p.draw();
+  }
 }
 function updateParticles() {
-        for (var i = particles.length - 1; i >= 0; i--) {
-            p = particles[i];
-            p.float();
-            p.boundaryCheck();
-        }
+  for (var i = particles.length - 1; i >= 0; i--) {
+    p = particles[i];
+    p.float();
+    p.boundaryCheck();
+  }
 }
 createParticles();
 drawParticles();
 function animateParticles() {
-        clearCanvas();
-        drawParticles();
-        updateParticles();
-        cloneCtx.drawImage(canvas, 0, 0);
-        requestAnimationFrame(animateParticles);
-    }
+  clearCanvas();
+  drawParticles();
+  updateParticles();
+  cloneCtx.drawImage(canvas, 0, 0);
+  requestAnimationFrame(animateParticles);
+}
 requestAnimationFrame(animateParticles);
 cloneCtx.drawImage(canvas, 0, 0);
 
-$(window).on('resize',function(){
+$(window).on('resize', function () {
   ww = $(window).width();
   wh = $(window).height();
   canvas.width = ww;
-  canvas.height= wh;
+  canvas.height = wh;
   clearCanvas();
   particles = [];
   createParticles();
   drawParticles();
 });
-function randomInt(min,max)
-{
-    return Math.floor(Math.random()*(max-min+1)+min);
+function randomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
 }
-function velocityInt(min,max)
-{
-    return Math.random()*(max-min+1)+min;
+function velocityInt(min, max) {
+  return Math.random() * (max - min + 1) + min;
 }
 
 function time() {
@@ -338,7 +336,7 @@ document.querySelector(".tombol").addEventListener('click', function () {
                                                     '',
                                                     'success'
                                                   ).then((result) => {
-
+                                                    window.location.href = "https://buattitadarirba.github.io/24valentine/tium/index.html";
                                                   })
                                                 })
                                               })
